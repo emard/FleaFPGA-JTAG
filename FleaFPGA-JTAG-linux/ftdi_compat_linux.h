8cf9ca4d34a81e2b65d37b707969efaa2e545e69
Emard <vordah@gmail.com>
2018-06-20 11:21:28 +0200

    ftdi_compat_linux.h: corrected comments

--- FleaFPGA-JTAG-linux/ftdi_compat_linux.h
+++ FleaFPGA-JTAG-linux/ftdi_compat_linux.h
@@ -1,8 +1,8 @@
 #ifndef FTDI_COMPAT_LINUX_H
 #define FTDI_COMPAT_LINUX_H
 
-// Windows FTDI compatibility defines
-// to use the same source as linux
+// Linux FTDI compatibility defines
+// to use the same source as windows
 
 #define	FT_CHECK(x)	(ftdi_status = (x), (ftdi_status >= 0 ? 0 : printf("FTDI: %s returned %d in %s:%d: %s\n", STR(x), (int32_t)ftdi_status, __FUNCTION__, __LINE__, ftdi_get_error_string(ftdi))), ftdi_status)
 #define FT_CHECK_WRITE(a,b,c,d) FT_CHECK(d = ftdi_write_data(a, b, c))
